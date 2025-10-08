9b5ffdbe5d2a75458ca3ac176d8cfba4b4ffa165
Karla-Michelle-Andres-Cruz <23308060610110@cetis61.edu.mx>
2025-10-08 15:21:08 -0600

    08/10/2025

--- app.py
+++ app.py
@@ -4,12 +4,23 @@ app = Flask (__name__)
 
 @app.route("/")
 def index():
-    arr = ["Luis", "Martin", "Rosa", "Paco", "Scott"]
-    autor = "Karla Michelle Andre Cruz"
-    return render_template("index.html", nombre = autor, amigos = arr)
+    return render_template("index.html")
 
+@app.route("/animales")
+def an():
+    return render_template("animales.html")
 
+@app.route("/autos")
+def aut():
+    return render_template("autos.html")
 
+@app.route("/7Maravillas")
+def Mara():
+    return render_template("maravillas.html")
+
+@app.route("/acercade")
+def acerca():
+    return render_template("acerca.html")
 
 
 
