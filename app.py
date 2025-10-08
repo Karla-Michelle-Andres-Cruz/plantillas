dd0728ce239c1235c27e6cad507aabaa9b32d8c1
Karla-Michelle-Andres-Cruz <23308060610110@cetis61.edu.mx>
2025-10-08 15:40:59 -0600

    08/10/2025

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ def index():
     return render_template("index.html")
 
 @app.route("/animales")
+def inicio():
+    return render_template("inicio.html")
+
+@app.route("/animales")
 def an():
     return render_template("animales.html")
 
